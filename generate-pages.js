1c217d48bc320de04adad4160ca7389c994ee8fd
HH <149450672+hamadahiro@users.noreply.github.com>
2026-03-17 22:27:08 +0000

    Improve sitemap: add priority and changefreq for better crawl guidance

--- generate-pages.js
+++ generate-pages.js
@@ -190,7 +190,7 @@ const cssVersion = cssVersionMatch ? cssVersionMatch[1] : '20260317b';
 
 // Track slugs to detect collisions
 const slugMap = {};
-const sitemapUrls = [`${SITE}/`];
+const sitemapEntries = [];
 let generated = 0;
 let skipped = 0;
 
@@ -212,20 +212,27 @@ data.calls.forEach(call => {
   slugMap[slug] = call.title;
   const html = generatePage(call, cssVersion);
   fs.writeFileSync(`${slug}.html`, html);
-  sitemapUrls.push(`${SITE}/${slug}`);
+  sitemapEntries.push({ url: `${SITE}/${slug}`, priority: '0.7', changefreq: 'monthly' });
   generated++;
 });
 
 // Generate sitemap.xml
 const today = new Date().toISOString().split('T')[0];
+const allEntries = [
+  { url: `${SITE}/`, priority: '1.0', changefreq: 'daily' },
+  ...sitemapEntries
+];
+
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${sitemapUrls.map(url => `  <url>
-    <loc>${url}</loc>
+${allEntries.map(e => `  <url>
+    <loc>${e.url}</loc>
     <lastmod>${today}</lastmod>
+    <changefreq>${e.changefreq}</changefreq>
+    <priority>${e.priority}</priority>
   </url>`).join('\n')}
 </urlset>`;
 
 fs.writeFileSync('sitemap.xml', sitemapXml);
 
-console.log(`Generated ${generated} pages, skipped ${skipped}, sitemap has ${sitemapUrls.length} URLs`);
+console.log(`Generated ${generated} pages, skipped ${skipped}, sitemap has ${allEntries.length} URLs`);
